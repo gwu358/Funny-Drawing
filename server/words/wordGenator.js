2c0410b65c69356fd6263780c46565281868bc82
gwu358 <gwu358@gmail.com>
2019-06-15 01:19:42 -0400

    finish word generator

--- server/words/wordGenator.js
+++ server/words/wordGenator.js
@@ -1,31 +1,22 @@
 const fs = require('fs');
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
 var list = [];
 let read = false;
-function readData(){
-  if(!read){}
-  fs.readFile('./easy.txt', 'utf8', function(error, data){
-    list[0] = data.split('\n');
-  })
-  
-  fs.readFile('./medium.txt', 'utf8', function(error, data){
-    list[1] = data.split('\n');
-  })
-  
-  fs.readFile('./hard.txt', 'utf8', function(error, data){
-    list[2] = data.split('\n');
-  })
-  
-  fs.readFile('./veryHard.txt', 'utf8', function(error, data){
-    list[3] = data.split('\n');
-  })
+async function readData(){ 
+  if(read) return;
+  list[0] = (await readFile('./easy.txt')).toString().split('\n')
+  list[1] = (await readFile('./medium.txt')).toString().split('\n')
+  list[2] = (await readFile('./hard.txt')).toString().split('\n')
+  list[3] = (await readFile('./veryHard.txt')).toString().split('\n')
+  read = true;
 }
 
 
 module.exports = function(difficult){
-  readData('some_path', function(data) {
-    if(!difficult) difficult = Math.floor(Math.random()*list.length);
+  return readData().then((data) => {
+    if(isNaN(difficult)) difficult = Math.floor(Math.random()*list.length);
     let li = list[difficult];
-    console.log(li)
     return li[Math.floor(Math.random()*li.length)];
-    } );
+  })
 }
